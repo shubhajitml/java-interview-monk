b9f9c7af1a82873c085434a362bd9fd1d5e92515
shubhajit <shubhajitdas121@gmail.com>
2024-11-10 19:54:01 +0530

    initial project files

---
+++ src/main/java/com/practice/javainterview/JavainterviewApplication.java
@@ -0,0 +1,13 @@
+package com.practice.javainterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavainterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavainterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/javainterview/JavainterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.javainterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavainterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
